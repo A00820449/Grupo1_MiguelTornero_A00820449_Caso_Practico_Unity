b5c937eb5746617fe78541e694f35d79f820760d
A00820449 <A00820449@itesm.mx>
2021-05-03 14:55:14 -0500

    everything

---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,48 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    Rigidbody rb;
+    float xInput;
+    public float speed;
+    public UnityEngine.UI.Text scoreboard;
+    int score;
+    // Start is called before the first frame update
+    void Start()
+    {
+        score = 0;
+        scoreboard.text = score.ToString();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        xInput = transform.position.x + Input.GetAxisRaw("Horizontal") * speed * Time.deltaTime;
+        xInput = Mathf.Clamp(xInput, -9f, 9f);
+        transform.position = new Vector3(xInput, -3.5f, 0f);
+    }
+
+    void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.CompareTag("Rock"))
+        {
+            score++;
+            Destroy(other.gameObject);
+        }
+        else if (other.gameObject.CompareTag("Power"))
+        {
+            transform.localScale = new Vector3(4f, 1f, 1f);
+            StartCoroutine(PowerDown());
+            Destroy(other.gameObject);
+        }
+        scoreboard.text = score.ToString();
+    }
+
+    IEnumerator PowerDown()
+    {
+        yield return new WaitForSecondsRealtime(6f);
+        transform.localScale = new Vector3(2f, 1f, 1f);
+    }
+}
