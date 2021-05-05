7da67cbbbb228be2f400496085620ca0603399c6
A00820449 <A00820449@itesm.mx>
2021-05-05 17:07:08 -0500

    added powered bool

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -4,16 +4,18 @@ using UnityEngine;
 
 public class PlayerController : MonoBehaviour
 {
-    Rigidbody rb;
+
     float xInput;
     public float speed;
     public UnityEngine.UI.Text scoreboard;
     int score;
+    bool powered;
     // Start is called before the first frame update
     void Start()
     {
         score = 0;
         scoreboard.text = score.ToString();
+        powered = false;
     }
 
     // Update is called once per frame
@@ -41,8 +43,13 @@ public class PlayerController : MonoBehaviour
 
     IEnumerator PowerUp()
     {
-        transform.localScale = new Vector3(4f, 1f, 1f);
-        yield return new WaitForSecondsRealtime(6f);
-        transform.localScale = new Vector3(2f, 1f, 1f);
+        if (!powered)
+        {
+            powered = true;
+            transform.localScale = new Vector3(4f, 1f, 1f);
+            yield return new WaitForSecondsRealtime(6f);
+            transform.localScale = new Vector3(2f, 1f, 1f);
+            powered = false;
+        }
     }
 }
